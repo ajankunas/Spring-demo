07d1a7a61b8f4fdcc226e4097308f85d09426766
Atas <atas.jankunas@gmail.com>
2023-09-25 16:18:27 +0300

    Add get person and get people methodes
    Add personRepository , add getters too person class

--- src/main/java/lt/techin/demo/entities/Person.java
+++ src/main/java/lt/techin/demo/entities/Person.java
@@ -1,19 +1,35 @@
 package lt.techin.demo.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "People")
 public class Person {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String firstName;
-
     private String lastName;
 
-}
+
+    //public void setFirstName(String firstName) {
+       // this.firstName = firstName;
+
+    //public void setLastName(String lastName) {
+        //this.lastName = lastName;
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    }
+
 
 
